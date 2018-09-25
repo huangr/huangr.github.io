1d277dd9615b5a8a95eff9f0e0110f480f97926e
Rick Huang <huangrickh@gmail.com>
2018-09-24 23:21:07 -0700

    Added awards section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,20 @@
 $(document).ready(function() {
 
-
-    $('#navbar a').smoothScroll({
-        speed: 'auto',
-        autoCoefficient: 1.5
-    });
+    console.log("Hello, world");
     
     adjust = function() {
-        if ($(window).scrollTop() <= 5) {
+        if ($(window).scrollTop() <= 100) {
             $('#navbar').removeClass('opaque');
         } else {
             $('#navbar').addClass('opaque');
         }
     }
-
-    // elems = ['#section-faq']
-
-    // elems.forEach(function(e) {
-    //  $(e).scrollspy({
-    //      min: $(e).offset().top,
-    //      onEnter: function(element, position) {
-    //          $($(element).attr('nav-link')).addClass('active');
-    //          console.log('enter', e)
-    //      },
-    //      onLeave: function(element, position) {
-    //          $($(element).attr('nav-link')).removeClass('active');
-    //      }
-    //  });
-    // });
     
     adjust();
 
     $(window).scroll(function(scroll) {
         adjust();
     });
+
+    $('.collapsible').collapsible();
 });
